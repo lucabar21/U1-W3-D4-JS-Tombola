f25b06419a0dc15a055bf81077f40f9970982478
lucabar21 <luca_barbuto@yahoo.it>
2024-02-01 16:47:34 +0100

    U1-W3-D4 - Tombola

---
+++ index.js
@@ -0,0 +1,33 @@
+const createNumbers = () => {
+  const container = document.getElementById("tombola-contanier");
+
+  for (let i = 0; i < 90; i++) {
+    const numCellDiv = document.createElement("div");
+    numCellDiv.classList.add("numeri");
+
+    const numCellH3 = document.createElement("h3");
+    numCellH3.innerText = i + 1;
+
+    numCellDiv.appendChild(numCellH3);
+    container.appendChild(numCellDiv);
+  }
+};
+
+const randomBtn = document.querySelector("button");
+
+const arrayNums = () => {
+  const extractedNumbers = [];
+  for (let i = 0; i < 90; i++) {
+    extractedNumbers.push(i + 1);
+  }
+  return extractedNumbers;
+};
+
+const randomNum = Math.floor(Math.random() * 90 + 1);
+randomBtn.onclick = () => {
+  extractedNumbers.push(randomNum);
+};
+
+window.onload = function () {
+  createNumbers();
+};
